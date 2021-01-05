160dbc1f4fcfabf442f248afa6f71cbd092e3ee4
Greg Ives <gregives774@gmail.com>
2021-01-05 00:26:19 +0000

    Handle undefined options

--- index.js
+++ index.js
@@ -3,10 +3,9 @@ const { parse } = require("postcss");
 const { parse: parseValue } = require("postcss-values-parser");
 const CleanCSS = require("clean-css");
 
-module.exports = async function (
-  content,
-  options = { import: true, url: true, minimize: false }
-) {
+module.exports = async function (content, _options) {
+  const options = { import: true, url: true, minimize: true, ..._options };
+
   const root = parse(content);
   dependencies = [];
 
